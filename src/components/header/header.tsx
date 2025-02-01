3164b7f693ed27fd151cb916feab138a21d784a8
siloksan <tutel07@gmail.com>
2025-02-01 13:44:03 +0300

    refactor: convert Header into functional component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,17 @@
-import { PureComponent } from 'react';
 import Logo from '../../shared/logo/logo';
 import { SearchBox } from '../search-bar/search-box';
 
 import styles from './header.module.scss';
 
 interface Props {
-  updateData: (name: string) => void;
+  readonly updateData: (name: string) => void;
 }
 
-export class Header extends PureComponent<Props> {
-  render() {
-    const { updateData } = this.props;
-    return (
-      <header className={styles.container}>
-        <Logo className={styles.logo} />
-        <SearchBox updateData={updateData} className={styles.search_box} />
-      </header>
-    );
-  }
+export function Header({ updateData }: Props) {
+  return (
+    <header className={styles.container}>
+      <Logo className={styles.logo} />
+      <SearchBox updateData={updateData} className={styles.search_box} />
+    </header>
+  );
 }
