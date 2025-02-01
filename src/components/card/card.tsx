949cf11dda908861439eb1afbf66eca2005d3b0a
siloksan <tutel07@gmail.com>
2025-02-01 14:27:24 +0300

    refactor: convert Card into functional component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,54 +1,48 @@
-import { PureComponent } from 'react';
-
 import { Spacecraft } from '../../api/types';
 import styles from './card.module.scss';
 
 interface Props {
-  cardInfo: Spacecraft;
+  readonly cardInfo: Spacecraft;
 }
 
-export default class SpaceCraftDetails extends PureComponent<Props> {
-  render() {
-    const {
-      cardInfo: {
-        name,
-        owner,
-        operator,
-        dateStatus = 'unknown',
-        spacecraftClass,
-        status = 'unknown',
-      },
-    } = this.props;
+export function Card({ cardInfo }: Props) {
+  const {
+    name,
+    owner,
+    operator,
+    dateStatus = 'unknown',
+    spacecraftClass,
+    status = 'unknown',
+  } = cardInfo;
 
-    const ownerName = owner ? owner.name : 'unknown';
-    const operatorName = operator ? operator.name : 'unknown';
-    const spacecraftClassName = spacecraftClass
-      ? spacecraftClass.name
-      : 'unknown';
+  const ownerName = owner ? owner.name : 'unknown';
+  const operatorName = operator ? operator.name : 'unknown';
+  const spacecraftClassName = spacecraftClass
+    ? spacecraftClass.name
+    : 'unknown';
 
-    return (
-      <li className={styles.container}>
-        <div className="ship-details">
-          <h2>
-            <strong>Name:</strong> {name}
-          </h2>
-          <p>
-            <strong>Owner:</strong> {ownerName}
-          </p>
-          <p>
-            <strong>Date of creation:</strong> {dateStatus}
-          </p>
-          <p>
-            <strong>Class:</strong> {spacecraftClassName}
-          </p>
-          <p>
-            <strong>Managed By:</strong> {operatorName}
-          </p>
-          <p>
-            <strong>Status:</strong> {status}
-          </p>
-        </div>
-      </li>
-    );
-  }
+  return (
+    <li className={styles.container}>
+      <div className="ship-details">
+        <h2>
+          <strong>Name:</strong> {name}
+        </h2>
+        <p>
+          <strong>Owner:</strong> {ownerName}
+        </p>
+        <p>
+          <strong>Date of creation:</strong> {dateStatus}
+        </p>
+        <p>
+          <strong>Class:</strong> {spacecraftClassName}
+        </p>
+        <p>
+          <strong>Managed By:</strong> {operatorName}
+        </p>
+        <p>
+          <strong>Status:</strong> {status}
+        </p>
+      </div>
+    </li>
+  );
 }
