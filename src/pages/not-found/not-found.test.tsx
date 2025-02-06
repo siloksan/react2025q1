8718e9dbdfb85d3cbbcbeeba0b2d593ac62024d8
siloksan <tutel07@gmail.com>
2025-02-06 17:04:24 +0300

    feat: add tests for NotFoundPage

---
+++ src/pages/not-found/not-found.test.tsx
@@ -0,0 +1,14 @@
+import NotFoundPage from './not-found';
+import { renderWithReactRouter } from '../../api/mock/mocks/mock-react-router';
+import { CLIENT_ROUTES } from '../../routes/routes';
+
+const renderSearchBox = renderWithReactRouter(NotFoundPage, CLIENT_ROUTES.HOME);
+describe('NotFoundPage', () => {
+  it('should render NotFoundPage', () => {
+    const screen = renderSearchBox(CLIENT_ROUTES.HOME);
+
+    const h1 = screen.getByRole('heading');
+
+    expect(h1).toBeInTheDocument();
+  });
+});
