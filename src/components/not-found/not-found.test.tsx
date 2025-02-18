cf0d1458c2921708d6b663b1182c0b5caad9b4bc
siloksan <tutel07@gmail.com>
2025-02-18 22:45:50 +0300

    refactor: tests for not found page

--- src/components/not-found/not-found.test.tsx
+++ src/components/not-found/not-found.test.tsx
@@ -1,14 +1,12 @@
-// import NotFoundPage from './not-found';
-// import { renderWithReactRouter } from '../../api/mock/mocks/mock-react-router';
-// import { CLIENT_ROUTES } from '../../routes/routes.constant';
+import { render } from '@testing-library/react';
+import NotFoundPage from './not-found';
 
-// const renderSearchBox = renderWithReactRouter(NotFoundPage, CLIENT_ROUTES.HOME);
-// describe('NotFoundPage', () => {
-//   it('should render NotFoundPage', () => {
-//     const screen = renderSearchBox(CLIENT_ROUTES.HOME);
+describe('NotFoundPage', () => {
+  it('should render NotFoundPage', () => {
+    const screen = render(<NotFoundPage />);
 
-//     const h1 = screen.getByRole('heading');
+    const h1 = screen.getByRole('heading');
 
-//     expect(h1).toBeInTheDocument();
-//   });
-// });
+    expect(h1).toBeInTheDocument();
+  });
+});
