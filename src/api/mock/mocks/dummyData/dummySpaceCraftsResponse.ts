147a51452ea0fc2ab215ca25b60f96d9ccc5f107
siloksan <tutel07@gmail.com>
2025-02-18 17:19:48 +0300

    refactor: card.test

---
+++ src/api/mock/mocks/dummyData/dummySpaceCraftsResponse.ts
@@ -0,0 +1,37 @@
+import { SpacecraftsResponse } from '../../../types';
+
+export const DUMMY_SPACECRAFTS_RESPONSE: SpacecraftsResponse = {
+  page: {
+    pageNumber: 0,
+    pageSize: 2,
+    totalElements: 2,
+    totalPages: 1,
+  },
+  spacecrafts: [
+    {
+      uid: 'test1',
+      name: 'SS Mariposa',
+      registry: 'NAR-7678',
+      status: 'Destroyed',
+      dateStatus: '22nd century',
+      spacecraftClass: null,
+      owner: null,
+      operator: {
+        uid: 'ORMA0000010494',
+        name: 'United Nations',
+      },
+      affiliation: null,
+    },
+    {
+      uid: 'test2',
+      name: 'SS Santa Maria',
+      registry: 'BDR-529',
+      status: 'Cannibalized',
+      dateStatus: '2360',
+      spacecraftClass: null,
+      owner: null,
+      operator: null,
+      affiliation: null,
+    },
+  ],
+};
