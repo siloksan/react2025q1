13332886477642207423a03c94848c550f5c8b04
siloksan <tutel07@gmail.com>
2025-02-22 22:29:12 +0300

    refactor: tests for pagination

--- src/components/pagination/pagination.test.tsx
+++ src/components/pagination/pagination.test.tsx
@@ -2,19 +2,35 @@ import { render } from '@testing-library/react';
 import { EMPTY_BUTTON_VIEW, Pagination } from './pagination';
 import userEvent from '@testing-library/user-event';
 import { PAGE_OFFSET } from '../cards-list/cards-list.constants';
+import { useQueryState } from '@/hooks';
+import { Mock } from 'vitest';
+import { QUERY_KEYS } from '@/constants';
+
+vi.mock('@/hooks', () => ({
+  useQueryState: vi.fn(),
+}));
 
 describe('Pagination', () => {
-  const handleClick = vi.fn();
+  const setQueryValue = vi.fn();
+
+  beforeEach(() => {
+    vi.spyOn(console, 'error').mockImplementation(() => vi.fn());
+    (useQueryState as unknown as Mock).mockReturnValue({ setQueryValue });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
 
   it('renders null if totalPages is less than 2', () => {
-    const props = { totalPages: 1, currentPage: 1, handleClick };
+    const props = { totalPages: 1, currentPage: 1 };
     const screen = render(<Pagination {...props} />);
 
     expect(screen.queryByTestId('pagination')).toBeNull();
   });
 
   it('renders pagination buttons correctly', () => {
-    const props = { totalPages: 5, currentPage: 3, handleClick };
+    const props = { totalPages: 5, currentPage: 3 };
 
     const screen = render(<Pagination {...props} />);
 
@@ -24,7 +40,7 @@ describe('Pagination', () => {
   });
 
   test('disables empty button value', () => {
-    const props = { totalPages: 10, currentPage: 3, handleClick };
+    const props = { totalPages: 10, currentPage: 3 };
 
     const screen = render(<Pagination {...props} />);
 
@@ -33,14 +49,15 @@ describe('Pagination', () => {
   });
 
   it('disables prev button on first page', () => {
-    const props = { totalPages: 10, currentPage: 1, handleClick };
+    const props = { totalPages: 10, currentPage: 1 };
 
     const screen = render(<Pagination {...props} />);
 
     expect(screen.getByText('prev')).toBeDisabled();
   });
+
   it('disables next button on last page', () => {
-    const props = { totalPages: 5, currentPage: 5, handleClick };
+    const props = { totalPages: 5, currentPage: 5 };
 
     const screen = render(<Pagination {...props} />);
 
@@ -48,7 +65,7 @@ describe('Pagination', () => {
   });
 
   it('disables current page button', () => {
-    const props = { totalPages: 5, currentPage: 3, handleClick };
+    const props = { totalPages: 5, currentPage: 3 };
 
     const screen = render(<Pagination {...props} />);
 
@@ -58,7 +75,9 @@ describe('Pagination', () => {
 
   it('should change current page number', async () => {
     const pageNumber = 4;
-    const props = { totalPages: 5, currentPage: 3, handleClick };
+    const newQueries = [{ key: QUERY_KEYS.PAGE, value: pageNumber.toString() }];
+
+    const props = { totalPages: 5, currentPage: 3 };
 
     const screen = render(<Pagination {...props} />);
 
@@ -67,12 +86,15 @@ describe('Pagination', () => {
     const user = userEvent.setup();
     await user.click(btn);
 
-    expect(handleClick).toHaveBeenCalledWith(pageNumber);
+    expect(setQueryValue).toHaveBeenCalledWith(newQueries);
   });
 
   it('should change current page into previous when prev btn is clicked', async () => {
     const currenPage = 3;
-    const props = { totalPages: 5, currentPage: currenPage, handleClick };
+    const props = { totalPages: 5, currentPage: currenPage };
+    const newQueries = [
+      { key: QUERY_KEYS.PAGE, value: (currenPage - PAGE_OFFSET).toString() },
+    ];
 
     const screen = render(<Pagination {...props} />);
 
@@ -81,12 +103,15 @@ describe('Pagination', () => {
     const user = userEvent.setup();
     await user.click(btn);
 
-    expect(handleClick).toHaveBeenCalledWith(currenPage - PAGE_OFFSET);
+    expect(setQueryValue).toHaveBeenCalledWith(newQueries);
   });
 
   it('should change current page into next when next btn is clicked', async () => {
     const currenPage = 3;
-    const props = { totalPages: 5, currentPage: currenPage, handleClick };
+    const props = { totalPages: 5, currentPage: currenPage };
+    const newQueries = [
+      { key: QUERY_KEYS.PAGE, value: (currenPage + PAGE_OFFSET).toString() },
+    ];
 
     const screen = render(<Pagination {...props} />);
     const btn = screen.getByText('next');
@@ -94,6 +119,6 @@ describe('Pagination', () => {
     const user = userEvent.setup();
     await user.click(btn);
 
-    expect(handleClick).toHaveBeenCalledWith(currenPage + PAGE_OFFSET);
+    expect(setQueryValue).toHaveBeenCalledWith(newQueries);
   });
 });
