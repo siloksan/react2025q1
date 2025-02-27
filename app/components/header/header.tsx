f0addc8e877519d6e1051bbe7f00738fba9a7190
siloksan <tutel07@gmail.com>
2025-02-27 21:52:32 +0300

    feat: add tests for header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,13 +1,12 @@
+import type { WithTestId } from '~/types';
 import { Logo } from '../shared/logo/logo';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
 
 import styles from './header.module.scss';
 
-export const HEADER_TEST_ID = 'header_test_id';
-
-export function Header() {
+export function Header({ testid }: WithTestId) {
   return (
-    <header className={styles.container} data-testid={HEADER_TEST_ID}>
+    <header className={styles.container} data-testid={testid}>
       <Logo className={styles.logo} />
       <ThemeToggle />
     </header>
